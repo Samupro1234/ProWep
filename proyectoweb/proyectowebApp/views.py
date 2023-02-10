fb598a16c3df41241e8f7d3c8616bce1161019da
Samupro1234 <samuelcompi1011@gmail.com>
2023-02-10 13:30:07 -0500

    sam

--- proyectoweb/proyectowebApp/views.py
+++ proyectoweb/proyectowebApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from proyectowebAppAforo.models import usuario
+from proyectowebAppAforo.models import usuario, guia, sitio, visitante
 # Create your views here.
 
 
@@ -7,17 +7,49 @@ def presentacion(request):
     return render(request, "proyectowebApp/presentacion.html")
 
 def sitios(request):
+    if request.method == "POST":
+        nombre = request.POST['nombre']
+        apellido = request.POST['apellido']
+        email = request.POST['email']
+        password = request.POST['password']
+        sitio = sitio(nombre=nombre, apellido=apellido, email=email, password=password)
+        sitio.save()
+        return render(request, "proyectowebApp/sitios.html")
     return render(request, "proyectowebApp/sitios.html")
 
 def guia(request):
+    if request.method == "POST":
+        nombre = request.POST['nombre']
+        apellido = request.POST['apellido']
+        email = request.POST['email']
+        password = request.POST['password']
+        guia = guia(nombre=nombre, apellido=apellido, email=email, password=password)
+        guia.save()
+        print("vjdsnvjdnvjdsamu")
+        return render(request, "proyectowebApp/guia.html")
+    
+
     return render(request, "proyectowebApp/guia.html")
 
 def usuario(request):
-    #user = usuarioss.objects.all()
-        
-    #request.post['nombre']
-    
+    if request.method == "POST":
+        nombre = request.POST['nombre']
+        apellido = request.POST['apellido']
+        email = request.POST['email']
+        password = request.POST['password']
+        usuario = usuario(nombre=nombre, apellido=apellido, email=email, password=password)
+        usuario.save()
+        return render(request, "proyectowebApp/usuario.html")
+        print('hola')
     return render(request, "proyectowebApp/usuario.html")
 
 def visitante(request):
+    if request.method == "POST":
+        nombre = request.POST['nombre']
+        apellido = request.POST['apellido']
+        email = request.POST['email']
+        password = request.POST['password']
+        visitante = visitante(nombre=nombre, apellido=apellido, email=email, password=password)
+        visitante.save()
+        return render(request, "proyectowebApp/visitante.html")
     return render(request, "proyectowebApp/visitante.html")
